0800247837542152f597a8277d20646a1f198524
Naman Kumar <naman.kumardmp@gmail.com>
2025-05-09 15:39:14 +0530

    Refactor Mistral API class to call API synchronously for PDF page chunks

--- class/mistralApi.js
+++ class/mistralApi.js
@@ -1,22 +1,49 @@
 require("dotenv").config();
-
-const fs = require("fs");
-const { z } = require("zod");
 const { Mistral } = require("@mistralai/mistralai");
+const ValidationClass = require("./validationClass");
+const mistralApiKey = process.env.MISTRAL_API_KEY;
 
 //----------------------------------------------class----------------------------------------------
 
 class MistralApiClass {
-  async mistralApi(filename) {
-    let loop = 1;
+  constructor() {
+    this.client = new Mistral({ apiKey: mistralApiKey });
+    this.validationClass = new ValidationClass();
+    this.cancel = false;
+  }
+  async mistralApi(pdfArray, filename) {
+    const promiseArray = [];
+    for (const index in pdfArray) {
+      promiseArray.push(
+        this.makeApiCall(pdfArray[index], `${filename} ${index + 1}`)
+      );
+    }
+
+    let result = [];
+    try {
+      result = await Promise.all(promiseArray);
+    } catch (err) {
+      this.cancel = true;
+      console.log(err);
+      return {
+        success: false,
+        error: "Please try again later.",
+      };
+    }
+
+    return {
+      success: true,
+      result: result,
+    };
+  }
+
+  async makeApiCall(uploadedFile, filename) {
+    if (this.cancel) {
+      throw new Error("Cancelled before start");
+    }
+    let result = { markdowns: [], imagesList: [] };
     let loopNo = 0;
-    const mistralApiKey = process.env.MISTRAL_API_KEY;
-
-    const client = new Mistral({ apiKey: mistralApiKey });
-
-    const uploadedFile = fs.readFileSync(`./public/pdf/${filename}`);
-
-    const uploadedPdf = await client.files.upload({
+    const uploadedPdf = await this.client.files.upload({
       file: {
         fileName: `${filename}`,
         content: uploadedFile,
@@ -24,47 +51,48 @@ class MistralApiClass {
       purpose: "ocr",
     });
 
-    const signedUrl = await client.files.getSignedUrl({
+    const signedUrl = await this.client.files.getSignedUrl({
       fileId: uploadedPdf.id,
     });
 
-    let markdowns = [];
-    let imagesList = [];
-
-    const mySchema = z.object({
-      index: z.number(),
-      markdown: z.string(),
-      images: z.any(),
-      dimensions: z.any(),
-    });
-    while (loop && loopNo < 3) {
+    while (loopNo < 3) {
+      if (this.cancel) {
+        throw new Error("Cancelled during processing");
+      }
       loopNo++;
-      const ocrResponse = await client.ocr.process({
-        model: "mistral-ocr-latest",
-        document: {
-          type: "document_url",
-          documentUrl: signedUrl.url,
-        },
-        includeImageBase64: true,
-      });
+      try {
+        const ocrResponse = await this.client.ocr.process({
+          model: "mistral-ocr-latest",
+          document: {
+            type: "document_url",
+            documentUrl: signedUrl.url,
+          },
+          includeImageBase64: true,
+        });
 
-      if (mySchema.safeParse(ocrResponse.pages[0]).success) {
-        loop = 0;
-
-        for (const page of ocrResponse.pages) {
-          markdowns.push(page.markdown);
-          imagesList.push(page.images);
+        if (this.validationClass.zValidation(ocrResponse.pages[0])) {
+          for (const page of ocrResponse.pages) {
+            result.markdowns.push(page.markdown);
+            result.imagesList.push(page.images.flat());
+          }
+          loopNo = 4;
         }
+      } catch (error) {
+        if (error.message.includes("rate") || error.response?.status === 429) {
+          const delay = 1000 * loopNo;
+          await new Promise((resolve) => setTimeout(resolve, delay));
+          continue;
+        } else {
+          console.log(error);
+        }
+      }
+
+      if (loopNo == 3) {
+        throw new Error("Please try again later");
       }
     }
 
-    if (!loop) {
-      return { success: true, markdowns: markdowns, imagesList: imagesList };
-    }
-    return {
-      success: false,
-      error: "Please try again later.",
-    };
+    return result;
   }
 }
 module.exports = MistralApiClass;
