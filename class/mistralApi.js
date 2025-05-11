7f7792f9cc85c34ad8d61350c534eb0cf472a288
naman kumar <naman.kumardmp@gmail.com>
2025-05-11 12:40:07 +0530

    refactor: pass individual PDF chunks to Mistral class and improve code readability
    - Updated logic to pass one element of the pdfChunk array at a time to the Mistral class
    - Refactored code for improved readability and maintainability

--- class/mistralApi.js
+++ class/mistralApi.js
@@ -1,22 +1,27 @@
 require("dotenv").config();
-
-const fs = require("fs");
-const { z } = require("zod");
 const { Mistral } = require("@mistralai/mistralai");
+const ValidationClass = require("./validationClass");
+const mistralApiKey = process.env.MISTRAL_API_KEY;
 
 //----------------------------------------------class----------------------------------------------
 
 class MistralApiClass {
-  async mistralApi(filename) {
-    let loop = 1;
-    let loopNo = 0;
-    const mistralApiKey = process.env.MISTRAL_API_KEY;
-
-    const client = new Mistral({ apiKey: mistralApiKey });
-
-    const uploadedFile = fs.readFileSync(`./public/pdf/${filename}`);
-
-    const uploadedPdf = await client.files.upload({
+  constructor() {
+    this.client = new Mistral({ apiKey: mistralApiKey });
+    this.validationClass = new ValidationClass();
+  }
+  async mistralApi(uploadedFile, filename) {
+    try {
+      const signedUrl = await this.uploadAndSignedUrl(uploadedFile, filename);
+      const result = this.makeApiCallWithRetries(signedUrl);
+      return result;
+    } catch (error) {
+      console.log(error);
+      throw new Error("Please try again later");
+    }
+  }
+  async uploadAndSignedUrl(uploadedFile, filename) {
+    const uploadedPdf = await this.client.files.upload({
       file: {
         fileName: `${filename}`,
         content: uploadedFile,
@@ -24,22 +29,39 @@ class MistralApiClass {
       purpose: "ocr",
     });
 
-    const signedUrl = await client.files.getSignedUrl({
+    const signedUrl = await this.client.files.getSignedUrl({
       fileId: uploadedPdf.id,
     });
+    return signedUrl;
+  }
+  async makeApiCallWithRetries(signedUrl) {
+    let loopNo = 0;
 
-    let markdowns = [];
-    let imagesList = [];
-
-    const mySchema = z.object({
-      index: z.number(),
-      markdown: z.string(),
-      images: z.any(),
-      dimensions: z.any(),
-    });
-    while (loop && loopNo < 3) {
+    while (loopNo < 3) {
       loopNo++;
-      const ocrResponse = await client.ocr.process({
+      try {
+        const result = await this.mistralApiCall(signedUrl);
+        return result;
+      } catch (error) {
+        if (error.message.includes("rate") || error.response?.status === 429) {
+          const delay = 1000 * loopNo;
+          await new Promise((resolve) => setTimeout(resolve, delay));
+          continue;
+        } else {
+          console.log(error);
+        }
+      }
+
+      if (loopNo == 3) {
+        throw new Error("Please try again later");
+      }
+    }
+  }
+
+  async mistralApiCall(signedUrl) {
+    try {
+      let result = { markdowns: [], imagesList: [] };
+      const ocrResponse = await this.client.ocr.process({
         model: "mistral-ocr-latest",
         document: {
           type: "document_url",
@@ -47,24 +69,16 @@ class MistralApiClass {
         },
         includeImageBase64: true,
       });
-
-      if (mySchema.safeParse(ocrResponse.pages[0]).success) {
-        loop = 0;
-
+      if (this.validationClass.zValidation(ocrResponse.pages[0])) {
         for (const page of ocrResponse.pages) {
-          markdowns.push(page.markdown);
-          imagesList.push(page.images);
+          result.markdowns.push(page.markdown);
+          result.imagesList.push(page.images.flat());
         }
+        return { imagesList: result.imagesList, markdowns: result.markdowns };
       }
+    } catch (error) {
+      throw new Error(error);
     }
-
-    if (!loop) {
-      return { success: true, markdowns: markdowns, imagesList: imagesList };
-    }
-    return {
-      success: false,
-      error: "Please try again later.",
-    };
   }
 }
 module.exports = MistralApiClass;
