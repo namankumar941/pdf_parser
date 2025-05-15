4b0be20326d4215a21c7be811adde89a0886a76d
naman kumar <naman.kumardmp@gmail.com>
2025-05-15 23:29:58 +0530

    feat: only one llm call instead of multiple llm call

--- class/handleAiApiSelection.js
+++ class/handleAiApiSelection.js
@@ -1,7 +1,6 @@
 const OpenAIClass = require("./openAiApi");
 const ClaudeAiApiClass = require("./claudeAiApi");
 const systemPrompt = require("../prompt/newFinalPrompt");
-const fs = require("fs");
 //----------------------------------------------class----------------------------------------------
 
 class HandleAiApiSelectionClass {
@@ -9,74 +8,25 @@ class HandleAiApiSelectionClass {
     this.claudeAiApiClass = new ClaudeAiApiClass();
     this.openAiApiClass = new OpenAIClass();
   }
-  async handleAiApiSelection(markdownChunks) {
-    console.log(markdownChunks.length);
+  async handleAiApiSelection(markdown, apiChoice) {
     try {
-      let promiseArray = [];
-      for (const chunk of markdownChunks) {
-        const contentIdentification = this.hasMarkdownElements(chunk);
-        if (contentIdentification.hasTable) {
-          promiseArray.push(
-            this.claudeAiApiClass.claudeApi(chunk, systemPrompt.finalPrompt)
-          );
-        } else if (
-          contentIdentification.hasCodeBlock ||
-          contentIdentification.hasInlineMath
-        ) {
-          promiseArray.push(
-            this.openAiApiClass.openAiApi(
-              chunk,
-              "gpt-4.1",
-              systemPrompt.finalPrompt
-            )
-          );
-        } else {
-          promiseArray.push(
-            this.openAiApiClass.openAiApi(
-              chunk,
-              "gpt-4.1",
-              systemPrompt.finalPrompt
-            )
-          );
-        }
+      if (apiChoice === "openai") {
+        const finalJson = await this.openAiApiClass.openAiApi(
+          markdown,
+          "gpt-4.1",
+          systemPrompt.finalPrompt
+        );
+        return finalJson;
+      } else {
+        const finalJson = await this.claudeAiApiClass.claudeApi(
+          markdown,
+          systemPrompt.finalPrompt
+        );
+        return finalJson;
       }
-      const promiseAllResponse = await Promise.all(promiseArray);
-      fs.writeFileSync(
-        "promiseAllResponse.json",
-        JSON.stringify(promiseAllResponse, null, 2),
-        "utf-8"
-      );
-      const finalJson = promiseAllResponse.reduce((merged, curr) => {
-        for (const key in curr) {
-          if (!merged[key]) merged[key] = [];
-          merged[key] = merged[key].concat(curr[key]);
-        }
-        return merged;
-      }, {});
-      return finalJson;
     } catch (error) {
-      console.log("111", error);
       throw new Error(error);
     }
-  }
-  hasMarkdownElements(markdown) {
-    const tableRegex =
-      /^ *\|?.+?\|.*\n *\|? *[:\-]+[:\-| ]*\n(?: *\|?.+\|.*\n?)*/gm;
-
-    const codeBlockRegex =
-      /(^ {4}.*(\n {4}.*)*)|(^\s*```[\s\S]*?```)|(^\s*~~~[\s\S]*?~~~)/gm;
-
-    const inlineMathRegex = /(?<!\$)\$(?!\$)([\s\S]*?[^\\])\$(?!\$)/g;
-
-    const blockMathRegex =
-      /(\$\$[\s\S]*?\$\$)|\\\[([\s\S]*?)\\\]|\\begin\{[a-zA-Z*]+\}[\s\S]*?\\end\{[a-zA-Z*]+\}/g;
-
-    return {
-      hasTable: tableRegex.test(markdown),
-      hasCodeBlock: codeBlockRegex.test(markdown),
-      hasInlineMath:
-        inlineMathRegex.test(markdown) || blockMathRegex.test(markdown),
-    };
   }
 }
 
